ace02e54ba28a576fcaadf91ce3d5999501b9758
Matthew M. Boedicker <matthewm@boedicker.org>
2013-07-31 22:26:28 -0700

    Add spec for implicit subject.

---
+++ spec/implicit_subject_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+class C
+end
+
+describe C do
+
+  it 'raises an error when subject is stubbed' do
+    expect {
+      subject.stub(method1: 'foo')
+    }.to raise_error(
+             /<C.* is the object under test and should not be stubbed/)
+  end
+
+end
